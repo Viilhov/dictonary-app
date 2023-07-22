5e0668d80c2f0e8c6fd9eed2de602a145adcd294
Viilhov <133661770+Viilhov@users.noreply.github.com>
2023-07-22 16:45:57 +0200

    Added axios, added dictonary css file and created api on order to make api calls

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,17 +1,26 @@
+import "./Dictionary.css";
 import React, { useState } from "react";
+import axios from "axios";
 
 export default function Dictionary() {
   const [keyword, setKeyword] = useState("");
+
+  function handleResponse(response) {
+    console.log(response.data);
+  }
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword}`);
+
+    let apiKey = "331711t40ba7f6a03e3o8bfc5f54faa4";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
   }
   function handleKeyword(event) {
     setKeyword(event.target.value);
   }
 
   return (
-    <div calssName="Dictionary">
+    <div className="Dictionary">
       <p>What would you like to look up?</p>
       <form className="dictionaryForm" onSubmit={search}>
         <input
